4ea5f2fb2b6eb74953d92aa7a94b65b8b735b395
Juan Alberto Zuluaga Ramirez <Beto14zuluaga@gmail.com>
2026-02-21 14:01:12 -0500

    Clase 01 getter y setter cons vacio y lleno

--- src/main/java/co/edu/cesde/ga/app/Main.java
+++ src/main/java/co/edu/cesde/ga/app/Main.java
@@ -2,6 +2,6 @@ package co.edu.cesde.ga.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!***");
+        System.out.println("Hello, World!");
     }
 }
